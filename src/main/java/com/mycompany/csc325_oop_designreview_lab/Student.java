8450a4272bbf9dfbb8f73da6447280247d5be971
Harpreet Singh <investorhs09@gmail.com>
2024-07-03 17:04:36 -0400

    All to do (without test)

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -8,12 +8,42 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Human
+// ToDo 1: Make this class a child of Human
+public class Student extends Human {
+
+    // ToDo 3: Add a field for GPA and create a setter and a getter
+    double GPA;
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    //initializing student by call constructor and adding the super class
+    public Student( String name, short age){
+        super(name,age);
+    }
+
+    // inherited from human class and overriding
+    @Override
+    public String getAddress() {
+        return null;
+    }
+
+
+    // inherited from human class and overriding
+    @Override
+    public void setAddress(String address) {
+
+    }
+
 
 	// ToDo 2: Fix the resulting errors
 
-	// ToDo 3: Add a field for GPA and create a setter and a getter
+
 	
 	// ToDo 4: Add comments to your code
 }
