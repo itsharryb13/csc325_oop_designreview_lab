e78306251c2945e8351b5027024e6f7db7ddfbb3
Harpreet Singh <investorhs09@gmail.com>
2024-07-03 17:57:37 -0400

    Commented and complete ( No Test)

---
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
@@ -0,0 +1,18 @@
+package com.mycompany.csc325_oop_designreview_lab;
+
+public class Freshman extends Student{
+
+    private int credits; // declaring the variable for credits
+    public Freshman(String name, short age , int credits) {
+        super(name, age); //Super class constructor
+
+        this.credits = credits; // initializing the variable
+
+
+    }
+
+    public String toString(){ // To String method which print the data in memory rather than address of the memory
+        // format the string to limit the use of concat and quotation marks
+        return String.format(" Senior : { Name : %s, Age = %o, GPA = %d, Credits = %o", getName(),getAge(),getGPA(),credits);
+    }
+}
