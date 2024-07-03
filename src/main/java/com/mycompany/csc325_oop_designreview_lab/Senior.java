a67a1fb663ad87dde52c35bf4483b0d34aab17d1
Harpreet Singh <investorhs09@gmail.com>
2024-07-03 17:47:14 -0400

    Commented and corrected Logic  ( No Test)

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Senior.java
@@ -1,20 +1,24 @@
 package com.mycompany.csc325_oop_designreview_lab;
 
 public class Senior extends Student {
+    //Minimum credits variable to get output the set minimum value
     private final int minCredits = 85; // created field for minimum credits
 
+
+    // Declaring the variable to store the input credit value
     private int credits;
     public Senior(String name, short age , int credits) {
-        super(name, age);
+        super(name, age); //Super class constructor
 
-        if ( credits < minCredits){
+        this.credits = credits; // initializing the variable
+
+        if ( credits < minCredits){ // making the needed comparison to be at least 85 credits.
             throw new IllegalArgumentException("Senior must have atLeast " +minCredits + " Credits." );
-        }else{
-            this.credits = credits;
         }
+
     }
 
-    public String toString(){
+    public String toString(){ // To String method which print the data in memory rather than address of the memory
         String output = String.format(" Senior : { Name : %s, Age = %o, GPA = %d, Credits = %o", getName(),getAge(),getGPA(),credits);
 
         return output;
